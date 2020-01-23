92cffdbdb08eac566e1055f411a339f40d87a71e
Felix <pgp1632000@gmail.com>
2020-01-22 23:53:11 -0500

    Added detailed description to each section and modified current version to be better

---
+++ background/get-started.php
@@ -0,0 +1,82 @@
+<?php
+define("DOCUMENT_ROOT", $_SERVER['DOCUMENT_ROOT']);
+include_once(DOCUMENT_ROOT."/php/constants.php");
+include_once(DOCUMENT_ROOT."/php/functions.php");
+?>
+
+<?php
+$title = "USING THE LIBRARY WITH STUDENTS";
+$pageName = "How I Got Started";
+include_once(DOCUMENT_ROOT."/includes/header.php");
+?>
+
+<?php include_once(DOCUMENT_ROOT."/includes/sidenav.php") ?>
+
+<div id="content">
+	<h1><?php echo $pageName ?></h1>
+	<h4>by Rudd Crawford</h4>
+
+  <p>“John, come over here and look at what you did.” </p>
+
+	<p>That was the spark that started it all.  We’re back in the 60’s at my first teaching job, in Concord, Massachusetts.  I had always liked brain-teaser math problems,
+    and had accumulated a small collection, stored on filing cards in a small green metal box, some supplied by my aunt Ruth.  I had a student named John who would now, no doubt,
+    be diagnosed with ADD.  He was a pleasant, easy-going kid who didn’t really get the hang of the math we were doing, largely because he couldn’t concentrate on it.
+    One day, on a whim, I gave him a piece of chalk, an eraser, and a problem to work on at the blackboard over in the corner of the room. The problem: Plant ten trees,
+    in five rows, with four trees in each row.  Once John understood that a tree could be in more than one row, he went to work.</p>
+
+  <p>He was persistent, which interested me to see, but after awhile he became frustrated, scribbled something on the board, and went and sat down.  I took a look at what
+    he’d scribbled and said, “John, come over here and look at what you did.”  He came over and saw that he had drawn a five-pointed star, which is the solution of the problem.
+    His face lit up: he’d solved it without knowing he’d done it.</p>
+
+  <p>The two takeaways for me were that he had uncharacteristically persisted in working on the problem, and that the solution had come up from somewhere inside of him,
+    independent of what he thought he was doing.</p>
+
+  <p>We fast forward to the mid 70’s in the Oberlin Middle School, where I taught for four years before a slot opened up at OHS.  I was given a “pre-algebra” class to teach to a group of very eager 7th and 8th graders, mixed.  There was no textbook.  I thought, “sheesh—learning about parentheses and My Dear Aunt Sally—they can learn that in ten minutes.”  So the whole and entire course was on the brain-teaser problems that I had continued to collect.  It was a great year—I just kept throwing problems at the kids; we’d do some in class and some were homework.  They ate ‘em up.</p>
+
+  <p>I gave them the problem about the infinite motel—room 1, room 2, room 3, on and on forever.  You’re the desk clerk.  All the rooms are full.  And your favorite
+    rock star shows up and wants a room.  What do you do?  The kids were stumped and I didn’t give them any help.  The reason that I remember this problem is that Sarah told me
+    that she came down to breakfast a morning or so later and her mom said, “Wow, Sarah, you were talking in your sleep, and you said the strangest things!”  Sarah said,
+    “Tell me what I said.”  “It was ‘just move them down a room, just move them down a room’, over and over.”  Sarah smacked her forehead and said “That’s it!  That’s the answer!”
+    And it was.  Her brain had been working on her behalf overnight.</p>
+
+  <p>So that was another spark for me, realizing the way the back alleyways of the brain could grab on to these problems and give some help.</p>
+
+  <p>We played around with that problem for awhile, and discovered that you, the desk clerk, could provide two rooms, or four, or a hundred.  In fact, if an infinitely long line of people wanted a room, they could be accommodated.  Just . . . </p>
+
+  <p>After another year or two I moved to the high school, and a year or two after that, there came this same group of students, now taking senior math.  They started right in on me: “We haven’t thought hard in math since middle school—give us some more of those problems.”</p>
+
+  <p>Hmmm, what to do?  By now, the kids were doing high school level contest problems, and some of them were really hard, for me too.  I didn’t know where I’d find the right stuff to give the kids, didn’t want to look stupid (I confess), so I just waffled on it—ducked the whole thing.</p>
+
+  <p>But there was Solomon, a senior with a luminous mind, brilliant in the way he could visualize hard concepts.  He and I did a lot of work after school together, as he wandered out beyond the edges of what high school kids usually learn. He was the worst—the crabbiest—about how he and his class weren’t getting hard enough work to do.  Nevertheless, I was a real fan of his, and was heartsick when he died in a bicycle accident before his first week of college classes.  He and I were close—I spoke at his memorial service, and his family gave me his light blue backpack, which I used for some years.</p>
+
+  <p>I thought about Solomon a lot.  I mused on the way we teachers always think that we’re giving our students valuable material to help them in their future lives.    I mused on how Solomon wasn’t going to have a future life.  And then the whole thing flipped around—the point wasn’t what I couldn’t do for Solomon’s life, but what his life could do for mine. And I realized that Solomon’s posthumous task was to goad me into giving the students the high school contest-level brain teasers that I’d been avoiding.</p>
+
+  <p>So I went to work and started digging out problems.  I got ahold of $100 and went to our local book warehouse, stocked up on problem books, and riffled through sets of contest problems and alluring brainteasers.  There are plenty of nifty problems out there to be found.</p>
+
+  <p>How to organize this?  I decided that the problem solving work would be on top of our regular curriculum, not slowing it down.  I decided not to try to find problems
+    that related to what we were studying at the time. I’d just find good stuff. Also, I didn’t want to be blamed personally for this increase in the work load.
+    So I hired a dear friend, a Dutch baroness with a deep love of problem solving, as a volunteer consultant to the math department.  I told the kids that the problems
+    were coming from her (they actually came from both of us but I was still distancing myself from this whole thing).  Her family nickname was Stella, and the problems were
+    Stella’s Stunners.</p>
+
+  <p>The kids took to it right away.</p>
+
+  <p>Then there was the process.  It quickly evolved to the complicated system I describe in the essay “Stella in my Classroom”.</p>
+
+  <p>The Stella program was a big hit with the kids, and I began finding problems for the other courses I was teaching.  My students told students in other classes what they
+    were doing, and dangled problems in front of them.  The other two teachers in the math department pricked up their ears, and eventually they asked me to supply problem sets
+    for their classes.  Fool that I was, I said sure I would, starting in the next fall.</p>
+
+  <p>Aghast, I realized what I had committed myself to doing: producing a problem set for each of the ten different courses in our math department, on a basically weekly basis.  Yikes!</p>
+
+  <p>I took a deep breath, maybe two.  For my single course, I had been browsing through problem books, finding problems that I could use (i.e. steal) or adapt for my students.  How could I do that for ten different courses?  I’d have to do my browsing for all ten courses at once.  I would need a storage system for what I found.  And so the grandly-named Stella Decimal System was born, patterned on the Dewey Decimal System.  I wrote down every kind of math problem I could think of and gave each one a number.  Then I could go through a problem book and give a number to each problem that I liked, and type them into my Apple II, organized by number. It was time consuming but doable.  I began printing the problems out on 5 x 8 filing cards, one card for the problem and another for its solution.  I quickly amassed a large number of cards (eventually more than a thousand problems).  Then for each course I could extract the cards I wanted, stack them up on the copy machine, and print out a master copy for the teacher.  I kept it up for the full school year, by which point each teacher had a year’s worth of problem sets.  </p>
+
+  <p>I never kept track of how much my colleagues were using their Stellas as the years went on, but she was a presence in my classroom until I retired in 2005.  In my classes of mixed achievement students I modified my process in various ways. As I say in the essay I mentioned above, I’m not totally happy with what I did.</p>
+
+  <p>During the years 1986-1998 I taught half-time in the mathematics department at Oberlin College.  My teaching partner and good friend Michael Henle and I co-taught mathematics courses for non-majors, and we wove Stellas through the course material.  The students’ write-ups were great reading. </p>
+
+  <p>Also during those years I was able to give problem solving workshops of various sorts to in-service teachers. We had a great time. </p>
+
+</div>
+
+<?php include(DOCUMENT_ROOT."/includes/footer.php") ?>
--- contact/index.php
+++ contact/index.php
@@ -4,7 +4,7 @@ include_once(DOCUMENT_ROOT."/php/constants.php");
 include_once(DOCUMENT_ROOT."/php/functions.php");
 ?>
 
-<?php 
+<?php
 $title = "Contact";
 $pageName = "Contact";
 include_once(DOCUMENT_ROOT."/includes/header.php");
@@ -15,7 +15,16 @@ include_once(DOCUMENT_ROOT."/includes/header.php");
 <div id="content">
 	<h1><?php echo $pageName ?></h1>
 
-	<p>This is where we will put contact information.</p>
+	<p>We would like to hear from you!  We welcome comments about the design of the website, in particular glitches/bugs that you find and suggestions to increase its user-friendliness.</p>
+
+	<p>We welcome comments about the problems—everything from typographical errors, difficulty in creating documents, to sharing alternate solutions you may have found.</p>
+
+	<p>We would like to hear about your students’ experiences in grappling with the Stellas.</p>
+
+	<p>We would like to hear your reflections on your own teaching, any ways of your own that you’ve found about using the Stellas
+	with your students, and how your perceptions of yourself as a teacher have evolved.</p>
+
+	<p>Send any messages to me, Rudd Crawford, at <a href="mailto:ruddac@hotmail.com">ruddac@hotmail.com</a>. I’ll try to get back to you promptly.</p>
 
 </div>
 
