950a0b8531db0ffe109d5e3444bd9596006b5dd6
Felix <pgp1632000@gmail.com>
2020-01-26 11:42:52 -0500

    remove the hyperlink.

--- tutorial/browse.php
+++ tutorial/browse.php
@@ -16,9 +16,9 @@ include_once(DOCUMENT_ROOT."/includes/header.php");
 <div id="content">
 	<h1><?php echo $pageName ?></h1>
 
-  <p>Go to <a href="/problems">Problems</a> (up top) and observe the list.  Scroll up and down and click on any problem that looks interesting (click on its number).
+  <p>Go to Problems and Document Creation (up top) and observe the list.  Scroll up and down and click on any problem that looks interesting (click on its number).
     Click on the solution. Do it a few more times. If you know a particular problem number or problem title you want to find, you can use Control F to find it
-    (it opens a box at the bottom of the page). Here are two samples to try:  <a href="http://stella.cs.oberlin.edu/problems/show-problem.php?id=3470.61">3470.61</a> and duck.</p>
+    (it opens a box at the bottom of the page). Here are two samples to try: 3470.61 and duck.</p>
 
 </div>
 
