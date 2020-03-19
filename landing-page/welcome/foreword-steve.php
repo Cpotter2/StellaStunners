42b1200f55f0c7bbd446d480b5ef99b3df76fad5
Colton Potter <cpotter2@oberlin.edu>
2020-03-19 15:02:00 -0400

    Add Steve's Foreword

--- landing-page/welcome/foreword-steve.php
+++ landing-page/welcome/foreword-steve.php
@@ -16,9 +16,305 @@ include_once(DOCUMENT_ROOT . "/includes/header.php");
 
 <div id="content">
   <h1><?php echo $pageName ?></h1>
-  <h4>by Steve</h4>
+  <h4>by Steve Meiring</h4>
 
-  <p></p>
+  <img src="foreword-steve-pic1.png" alt="&ldquoWe are all a little weird and
+  Life's a little weird, And when we find someone whose Weirdness is
+  compatible with ours, We join up with them and fall in Mutual weirdness and
+  call it Love (of Mathematics).&rdquo --Dr. Seuss" width="50%" style="
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+" />
+
+  <p>What better way to introduce you to the world of <span style="font-style: italic">Stella’s Stunners</span> than
+    to let you know at the outset you have entered a realm where the usual
+    rules of learning seem not always to apply. Somehow, you found your way to
+    this website, presumably because you have a strong interest in problem
+    solving. Or perhaps your arrival here is motivated by the desire to
+    stimulate deeper and more thoughtful mathematical interactions for your
+    students through good problems. If so, you have come to the right
+    location.</p>
+
+  <p style="
+    padding: 5px;
+    background: #ffffaf;
+    border: 1px solid red;
+    margin: 0% 5%;
+  ">This introductory essay is an electronic orientation for what you can
+    expect to find here and to provoke your desire to explore all that is to
+    be discovered.</p>
+
+  <p>To start: A <span style="font-style: italic">Stunner</span> is a non-routine problem like the following:</p>
+
+  <img src="foreword-steve-pic2.jpg" alt="A 2-pan balance with 4 brass
+  weights sitting in front of it." style="float: right; width: 25%">
+
+  <p style="padding: 2%">Ignacio runs the Zaragoza Deli in New York City. He has a 2-pan
+    balance and a set of 4 brass weights. He can weigh out any whole number
+    of ounces up to 40 using just these weights. What is the amount of each
+    brass weight? (Weights are used in any combination on either or both pans of
+    the scale.)</p>
+
+  <p>Problems provide the medium for exchanging ideas in mathematics. The
+    better the problems, the more intriguing are discussions and discoveries
+    that prompt the thinking that leads to solution ‒ as well as implications
+    for how that thinking is directed toward other problems. Even more compelling
+    are discoveries we make about our own thinking processes, and how we are
+    changed by the experience of confronting good problems.</p>
+
+  <p>Being human, we react to challenges in which we invest time. Our
+    reactions might be relief at being finished, elation at success, self-doubt
+    for admitting failure, enlightenment of discovery, agitation because the
+    result was not worth the effort, or myriad other emotions unique to the
+    individual. The most important reaction occurs when a problem takes on a
+    life of its own. The solution search no longer driven by someone else’s
+    direction, it becomes a personal drive for understanding and reflection. </p>
+
+  <p>The point is that interesting, fanciful, challenging problems command our
+    attention over an appreciable time span, and in the process become
+    memorable. Good problems fascinate us; they provoke intriguing
+    conversations; and they tag along as unbidden companions as we go about
+    other activities. Once the challenge of a good problem has gotten hold of you,
+    you can’t let it go. <span style="font-style: italic">Stunners</span> make mathematics a more fun activity in which
+    to become personally invested to learn.</p>
+
+  <p>In that context, this site is a repository of more than 700 such problems,
+    assembled by now retired teacher Rudd Crawford, over the course of a long
+    teaching career, which started in Concord, Massachusetts and which ended in
+    the small Ohio college town of Oberlin. Used with students for much of his
+    teaching career, these non-routine problems brought a new sense of purpose
+    to his students’ learning and, in the process, helped to enrich the
+    traditional program.</p>
+
+  <p>Of consequence to you, this problem collection is offered as a
+    professional service. Problems are free for the taking. The collection is
+    organized by course, by title, by topic, and into problem sets used by Rudd
+    and his Oberlin HS colleagues to nurture student problem solving on a
+    recurring basis. One of many features of this site is a selection basket
+    for which the web visitor can choose particular sets of 6-8 problems from
+    the collection itself or self-select individual problems, assemble, and
+    print out a personalized problem set. More about these organizational
+    features in the section, <span style="font-style: italic">The Problem Library.</span> </p>
+
+  <h2>An Odyssey of Teaching Discovery</h2>
+
+  <p>Accompanying the <span style="font-style: italic">Stella</span> collection is a series of short essays written by
+    Rudd in his authentic and understated teaching style. These essays are
+    found under the Post-It sections Using the Library with Students and
+    Everything Else. The essays tell a story of the discoveries made by a
+    classroom teacher, eventually leading to how to use <span style="font-style: italic">Stellas</span> (student name
+    for <span style="font-style: italic">Stunners</span>) to complement the normal fare of textbook learning. </p>
+
+  <p>The story begins in the late 1960s with Rudd making the discovery that
+    his personal affection for brain-teaser problems, and small collection on
+    note cards, could be used with a student when nothing else seemed to work.
+    In <span style="font-style: italic">How I Got Started</span>, we learn how this notion stayed with him into the
+    1970s, whereupon he confronted junior high school students who were bright,
+    but bored, with the customary textbook work. Once again, he found that
+    learners flourished in an environment liberally sprinkled with non-routine
+    problems. </p>
+
+  <p>When they (and he) eventually reached the high school together, those same
+    students clamored, “We haven’t done anything hard in math since middle school
+    – give us some more of those problems.” This impetus and a student named
+    Solomon caused Rudd to begin pondering. That pondering led to how to offer a
+    new, problem-solving dimension within the framework of traditional learning –
+    practically, instructionally, and programmatically. It also led Rudd to think
+    deeply about the nature of mathematics, how students learn it, and how they
+    are shaped by that learning.</p>
+
+  <p>In the essay, <span style="font-style: italic">Stella’s Biography</span> we make the acquaintance of Stella, an older
+    and eccentric Dutch baroness − friend to Rudd and, up to the time of her
+    death, a consultant to the Oberlin HS math department. Among her many
+    eccentricities was a penchant for carrying around vexing problems in her
+    leather purse until they were solved, and even after, in a sort of
+    celebration. Stella became the source of the stunners visited upon students.
+    She also served as a model for students of a mathematician who relishes the
+    challenge of good problems and who will persist with them until they are
+    vanquished.</p>
+
+  <p><span style="font-style: italic">Stella in My Classroom</span> recounts how <span style="font-style: italic">Stellas</span> came to be offered in sets of 6-8
+    problems considered independently by students over the course of the calendar
+    week − besides regular coursework. By week’s end, solutions and student
+    thoughts about the stunners were recorded in their notebooks and turned in for
+    comments and grading. This essay further describes how students came to
+    understand that <span style="font-style: italic">heuristics</span> arise naturally as what one does when you don’t
+    even know how to start on a problem. </p>
+
+  <p>The section on grading is a concession to the reality that a ‘good’
+    performance on any problem set is about half the problems solved. It also
+    presents an alternative scheme (and philosophy) for how to evaluate problem
+    solving in a way that is constructive and not self-defeating. </p>
+
+  <h2>The Argument for <span style="font-style: italic">Stellas</span></h2>
+
+  <p>Perhaps the most important essays about teaching discovery are <span style="font-style: italic">What Students
+      Learn from Problem Solving</span> and <span style="font-style: italic">Feedback from Former Students</span>. “What Students
+    Learn …” is an insightful look at the purpose for learning mathematics as
+    measured against how we typically go about it. Implied without it being stated
+    is this generalization: we usually learn mathematics, lesson after lesson, in
+    a manner that depends upon the teacher to predigest the material for us, to
+    work out sample problems, and then to assign exercises that coincidentally
+    happen to fit the lesson just taught. When difficulties are encountered,
+    students are encouraged to quickly seek help. The pared down problems and
+    exercises are mostly bereft of the contexts that often make problems more
+    interesting.</p>
+
+  <p>This kind of a lesson is efficient for learning lots of mathematics and to
+    automate the skills, ideas, and processes to be acquired. Doing so frees up
+    the mind to learn new things the next day. The limitation is that the
+    knowledge acquired is customarily at an introductory level. It is not
+    immediately integrated into a deeper and well-connected hierarchy of
+    understanding that makes that knowledge quickly and easily accessible to
+    address new problems presented in a different context. Consider, for example,
+    this problem:</p>
+
+  <p style="padding: 2%">A chicken and a half lays an egg and a half in a day and a half. How many
+    eggs will 12 chickens lay in 12 days?</p>
+
+  <p>It is safe to say that most secondary students are not very good at solving
+    such a whimsical problem. The question is Why not? And what does that tell us
+    about the relative accessibility of their learning?</p>
+
+  <p>One answer is that dispositions the student brings to the task influence her
+    chances for success. Students are accustomed by daily lessons to think of math
+    as a task or situation you identify as something recognized, for which you
+    recall from memory an appropriate technique for solution. This preconception
+    applied to the above problem presents a conundrum. It is not recognizable.
+    Habituated to dispatching such a task quickly and routinely, you take a
+    superficial ‘swipe’ at solution, multiplying the information in the first
+    sentence through by 2/3 . Eventually, you arrive at the incorrect answer of
+    12, or a more reasonable, yet still wrong result of 144.</p>
+
+  <p>Missing in this pedagogical sequence is the opportunity for students to learn
+    <span style="font-style: italic">HOW</span> to address situations for which they have not been explicitly taught. This
+    goal entails learning the type of heuristic thinking and learning skills
+    necessary to apply what you understand to something new. <span style="font-style: italic">Stellas</span> offer that
+    opportunity in the equivalent of a ‘virtual lab,’ where the emphasis is on
+    thinking your way through to a solution, rather than the mechanics or elegance
+    of the solution steps. <span style="font-style: italic">Stellas</span> provide a new and complementary dimension to
+    textbook-style learning.</p>
+
+  <p><span style="font-style: italic">Extending Problems</span> raises two other considerations for <span style="font-style: italic">Stellas</span>. First, good
+    problems often beget other good problems to solve. Take the example of the
+    balance problem above. Why are the required weights all powers of 3? This is a
+    suitable problem for students and teacher alike.</p>
+
+  <p>Extending problems provides an opportunity for the Stella strand and regular
+    coursework to intersect. A Stella problem can foreshadow a textbook lesson to
+    come, or it can revisit a previous topic learned, but in a different way. For
+    example, the chicken-and-egg problem could be identified as an extension of
+    direct variation. Suppose the student begins by exploring the problem and
+    mulls, “If I double the number of chickens, what should happen to the number
+    of eggs produced?” This quickly leads to a method of solution for building up
+    the ratios until the required condition is met. This is an illustration of
+    using Stella problems to revisit previously learned ideas and to extend
+    students’ understanding.</p>
+
+  <p>After the chicken problem has been considered, perhaps now recognized as a
+    direct variation of two independent variables, it sets the stage for a
+    subsequent textbook lesson on compound variation. In the midst of that
+    discussion, the teacher can innocently pose this question, “Where have you
+    seen an example of a compound variation that depends on two independent
+    variables and a constant rate?” One answer (i = P ∙ r ∙ t) presents another
+    avenue for solution to the chicken-and-egg problem as well as a connection
+    to a taught schema for solving a class of problems.</p>
+
+  <p>Next, we come to the essay on <span style="font-style: italic">Feedback from Former Students</span>. As modern
+    research will attest, emotions often influence how we think as much as do our
+    reasoning skills. What we believe to be true (whether a problem can be solved
+    by us), what we think is an appropriate learning task (is it fair to ask me to
+    start out not knowing what to do), and what importance we assign to a task
+    (our passion to persist in spite of adversity) are direct measures of how much
+    of our sense of self and our intellectual resources we will commit to the
+    task. </p>
+
+  <p><span style="font-style: italic">Stellas</span> present a learning environment in which the rules of engagement are
+    identified upfront as different from textbook exercises. By definition,
+    <span style="font-style: italic">Stellas</span> are hard; you are expected to persist over a period of time; and not
+    knowing how to proceed is a given. Sometimes you get the problem; and
+    sometimes the problem gets you. Students are encouraged (and expected) to
+    express their feelings in their writeups about <span style="font-style: italic">Stellas</span> and themselves after
+    they have emerged from the experience of tackling something that is mentally
+    challenging.</p>
+
+  <h2>The Gestalt of <span style="font-style: italic">Stellas</span></h2>
+
+  <p>Science education as a discipline has long had a handle on a unifying
+    principle they define as <span style="font-style: italic">scientific inquiry</span>. In a nutshell, it means that
+    science has adopted the notion that for students to learn science
+    appropriately, they sometimes must have opportunities where they must think
+    like scientists:</p>
+
+  <p style="padding: 2%">Inquiry means that students are handling science; they are manipulating it,
+    working it into new shapes and formats, integrating it into every corner of
+    their world, and playing with it in unknown ways. Inquiry implies that
+    students are in control of an important part of their own learning where they
+    can manipulate ideas to increase understanding. As students learn to think
+    through the designs and developments of their own inquiry, they also develop a
+    sense of self-responsibility that transcends all subject areas.</p>
+
+  <p> What Rudd’s Stella strand provides are opportunities for students to apply
+    what they have learned and sometimes to discover mathematics as it was
+    created. They have the occasional chance to act like mathematicians in the
+    virtual laboratory world of <span style="font-style: italic">Stellas</span>. That experience changes them in the way
+    in which they think. They learn habits for learning like Persistence,
+    Resilience, Focusing over time, Flexibility, Curiosity, and Love of subject
+    matter.</p>
+
+  <p>As quotes from Crawford’s former students attest: <span style="font-style: italic">Stellas</span> taught them more
+    than just how to do and how to learn mathematics. <span style="font-style: italic">Stellas</span> taught them how to
+    apply their minds generally.</p>
+
+
+  <p>In looking over the various essays and problems that comprise <span style="font-style: italic">Stella’s
+      Stunners</span>, one is struck by several things. One, none of us gets smart all at
+    once – students or the teacher. It took the better part of a teaching career
+    for Rudd to refine the Stunners you will find here and the teaching
+    methodologies that support them. At each and every turn in the essays, you
+    find illustrations of teaching methods that are dead on. </p>
+
+  <p>For example, problems were given out as sets rather than individually (a
+    <span style="font-style: italic">Stella</span> used as a class toss-up question primarily benefits the solver).
+    Assigned sets were constructed to give every student a chance to participate
+    and some measure of success. Sets worked on over a span of days enabled
+    students to learn what persistence looks like and how their minds work when
+    given time and sufficient focus. He employed an evaluation scheme that
+    validated the importance of the work and that assigned credit in a
+    constructive way that valued alternative approaches. </p>
+
+  <p>He created a safe environment where students could struggle and meet success
+    or failure in applying their knowledge, knowing that they were not competing
+    against everyone else, but only against what they themselves were capable.
+    Requiring formal writeups of problems taught students how to do a technical
+    report. Stella, herself, was a model for student expectations as well as the
+    nemesis toward whom students could focus their vexations.</p>
+
+
+  <p>None of this came about easily nor quickly. In one sense, Rudd had to
+    practice what he expected of his students. Just as they had to learn how to
+    learn differently, he had to learn how to think differently about teaching. He
+    started slow. He experimented with better students first. He learned how to
+    adapt and to adjust to what worked and what didn’t. He talked with other
+    colleagues who were interested in the same things. This is one more serious
+    piece of advice for you, the reader, in experimenting with these materials.</p>
+
+
+  <p>But mostly, the essays are a story of a love affair among a teacher, his
+    students, and challenging, quirky math problems. One doesn’t put that much
+    energy and time into something requiring this degree of hard work and
+    creativity without loving what you are doing. That same affection for his
+    profession and for what good teaching should aspire to has led to this
+    resource of <span style="font-style: italic">Stellas</span> made available to you, the website visitor.</p>
+
+  <br>
+  <p>Steve Meiring has been a long-time friend of the Stella program and himself
+    has a life-long interest in problem solving. Now retired, he has conducted
+    hundreds of workshops on problem solving and authored <span style="font-style: italic">Problem Solving…a basic
+      mathematics goal I & II</span> as a mathematics consultant for the Ohio Department of
+    Education. He is also the author of an unpublished manuscript, <span style="font-style: italic">ThinkAbout It:
+      A Resource Guide for Secondary Math Teachers and Students</span>.</p>
 
 </div>
 
